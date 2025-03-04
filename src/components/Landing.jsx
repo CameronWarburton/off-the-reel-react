e3a2b55d959a11bd9b983695c532bf567522d953
Cameron <cameron.cw@outlook.com>
2025-03-04 13:09:10 +0000

    Importing relevant hooks

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
+import { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 const Landing = () => {
   return (
